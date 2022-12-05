a11a0aea152c00bee5d0e9e1264511037c2a09f3
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-12-05 16:33:24 -0500

    created cars app + created car model for cars app + registered cars and rest framework in project settings installed app section

--- cars_project/settings.py
+++ cars_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gw!#gj_r6b#@gs=y*sqy5fk86sig$m_()ne)k&5y=2(f9i$+)x'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'cars',
 ]
 
 MIDDLEWARE = [
@@ -68,21 +67,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'cars_project.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'cars_database',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'jonjon87',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
